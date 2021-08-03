de3c30c67ff02a56a77623e9c0a77732dc98a03b
Yunsheng <jiang-yunsheng@hotmail.com>
2021-08-03 10:55:04 +0800

    fix: rect style fix

--- src/customComponents/Rect.tsx
+++ src/customComponents/Rect.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { CompDataItem } from '../store/interfaces';
 import Text from './Text';
+import styles from './index.module.less';
 
 const Rect: React.FC<CompDataItem> = (compDataItem) => (
-  <div>
+  <div className={styles.Rect}>
     <Text {...compDataItem} />
   </div>
 );
