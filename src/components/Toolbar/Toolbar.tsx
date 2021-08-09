73f5bb7efcab213e9dcc60068fd846516fe8ec04
Yunsheng <jiang-yunsheng@hotmail.com>
2021-08-09 17:22:51 +0800

    feat: support export to PDF

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -16,11 +16,26 @@ const ToolBar: React.FC = () => {
     window.localStorage.setItem('data', JSON.stringify(snapshotData[snapshotIndex]));
     message.success('Saved!');
   };
+
+  const exportToPDF = () => {
+    const content = document.querySelector('#stageContent');
+    const height = content?.clientHeight;
+    const width = content?.clientWidth;
+    const contentString = content?.outerHTML;
+    const printWindow = window.open('', '', `height=${height},width=${width}`);
+    printWindow?.document.write('<html><head><title>diagram</title>');
+    printWindow?.document.write('</head><body>');
+    printWindow?.document.write(contentString || '');
+    printWindow?.document.write('</body></html>');
+    printWindow?.document.close();
+    printWindow?.print();
+  };
   return (
     <Card style={{ height: '5rem' }}>
       <Button disabled={!canUndo} onClick={undo}>undo</Button>
       <Button disabled={!canRedo} onClick={redo}>redo</Button>
       <Button onClick={save}>save</Button>
+      <Button onClick={exportToPDF}>export to PDF</Button>
     </Card>
   );
 };
